2aa82c03f97ce0f8536e50a638515017e1fe2177
Javlonbek Saydullayev <88250263+Saydullayev017@users.noreply.github.com>
2023-11-27 07:05:05 +0300

    added styles for basket and fixed index.js and other

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,9 @@ const cartItemsContainer = document.getElementById("cart-items");
 const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 // Добавил прослушиватель кликов для каждой кнопки «Добавить в корзину».
-addToCartButtons.forEach(button => {
+addToCartButtons.forEach((button) => {
   button.addEventListener("click", () => {
-// Получил информацию о продукте
+    // Получил информацию о продукте
     const card = button.closest(".card");
     const name = card.querySelector(".card-name").innerText;
     const price = card.querySelector(".price").innerText;
@@ -21,10 +21,12 @@ addToCartButtons.forEach(button => {
 });
 
 // Отображение содержимого корзины при загрузке страницы
-cart.forEach(item => {
+cart.forEach((item) => {
   const cartItem = document.createElement("div");
-  const image = item.image ? `<img class="product-img" src="${item.image}" alt="${item.name}">` : "";
-  
+  const image = item.image
+    ? `<img class="product-img" src="${item.image}" alt="${item.name}">`
+    : "";
+
   cartItem.innerHTML = `
     <div>Name: ${item.name}</div>
     <div>Price: ${item.price}</div>
@@ -33,3 +35,61 @@ cart.forEach(item => {
 
   cartItemsContainer.appendChild(cartItem);
 });
+
+// Заново получить корзину из localStorage
+
+// Функция для обновления корзины в localStorage
+const updateCart = (cart) => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+
+// Отображение содержимого корзины по загрузке страницы
+const displayCartItems = () => {
+  cartItemsContainer.innerHTML = ""; // Очистить контейнер перед отображением элементов
+
+  cart.forEach((item, index) => {
+    const cartItem = document.createElement("div");
+    cartItem.classList.add("cart-item");
+    const image = item.image
+      ? `<img class="product-img" src="${item.image}" alt="${item.name}">`
+      : "";
+
+    cartItem.innerHTML = `
+      <div>Name: ${item.name}</div>
+      <div>Price: ${item.price}</div>
+      ${image}
+      <div>
+        <button class="remove-item-button" data-index="${index}">Удалить</button>
+        <button class="increase-quantity-button" data-index="${index}">Увеличить</button>
+      </div>
+    `;
+
+    cartItemsContainer.appendChild(cartItem);
+  });
+
+  // Добавить прослушиватель событий для кнопок удаления товара и увеличения количества
+  const removeItemButtons = document.querySelectorAll(".remove-item-button");
+  removeItemButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const index = button.dataset.index;
+      cart.splice(index, 1); // Удалить элемент из корзины
+      updateCart(cart); // Обновить корзину в localStorage
+      displayCartItems(); // Перерисовать корзину
+    });
+  });
+
+  const increaseQuantityButtons = document.querySelectorAll(
+    ".increase-quantity-button"
+  );
+  increaseQuantityButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const index = button.dataset.index;
+      cart[index].quantity = (cart[index].quantity || 1) + 1; // Увеличить количество товара
+      updateCart(cart); // Обновить корзину в localStorage
+      displayCartItems(); // Перерисовать корзину
+    });
+  });
+};
+
+// Отображение содержимого корзины при загрузке страницы
+displayCartItems();
